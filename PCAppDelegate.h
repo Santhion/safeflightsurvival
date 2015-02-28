ab4f6197e1f4c84b98972e04587502614bc9d7b9
Santhion <kevin.mcready@yahoo.com>
2015-02-28 16:21:38 -0500

    Create PCAppDelegate.h

---
+++ PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  Safe Flight
+//
+//  Created by Kevin McReady on 10/1/14.
+//  Copyright (c) 2014 Epiphany!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
